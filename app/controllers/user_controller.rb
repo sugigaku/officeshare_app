67ffa1404b8852549572d9ab77b1c74b10a27a63
sugigaku <gakuya124ftb@gmail.com>
2020-01-10 17:43:45 +0900

    諸々変更

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,8 +5,6 @@ class UserController < ApplicationController
   def new_student
   end
 
- 
-
   def create_student
     @user=User.new(
       belongs:"student",
@@ -17,12 +15,8 @@ class UserController < ApplicationController
       grade: params[:grade], 
       profile: params[:profile],
       icon: "user_default.png")
-
-  
-    
    if @user.save
     session[:user_id]=@user.id
-   
     redirect_to("/user/student/#{@user.id}")
    else
     @error_message="入力されたユーザーIDが既に使用されているまたはパスワードが入力されていないため登録できません"
@@ -30,11 +24,12 @@ class UserController < ApplicationController
     @password= params[:password]
     render("user/new_student")
    end
-   
   end
+
 
   def new_company
   end
+
 
   def create_company
     @user=User.new(
@@ -54,33 +49,27 @@ class UserController < ApplicationController
     @password= params[:password]
     render("user/new_company")
    end
-
   end
 
 
-  def student_mypage
+  def mypage
    @user=User.find_by(id: session[:user_id])
-   
   end
 
-  def company_mypage
-    @user=User.find_by(id: session[:user_id])
+
+  def new_post
   end
+
 
   def login_form
-    
   end
-#studentとcompanyの書き分け残ってる
+
+
   def login
     @user=User.find_by(userid: params[:userid], password: params[:password])
     if @user
       session[:user_id]=@user.id
-  #この辺適当
-      if @user.belongs="student"
-        redirect_to("/user/student/#{@user.id}")
-      else
-        redirect_to("/user/company/#{@user.id}")
-      end
+      redirect_to("/user/#{@user.id}")
     else
       @error_message="ユーザIDまたはパスワードが間違っています"
       @userid= params[:userid]
@@ -89,11 +78,13 @@ class UserController < ApplicationController
     end
   end
 
+
   def logout
    session[:user_id]=nil
    redirect_to("/login")
   end
 
+  
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
  post "user/create_student"=>"user#create_student"
  get "user/new_company"=>"user#new_company"
  post "user/create_company"=>"user#create_company"
-  get "user/student/:id"=>"user#student_mypage"
-  get "user/company/:id"=>"user#company_mypage"
+  get "user/:id"=>"user#mypage"
+  
   
   get "/login"=>"user#login_form"
   post "/login"=>"user#login"
   #postでログアウトできてないまま、getで暫定的に対応
   post "/logout"=>"user#logout"
   get "/logout"=>"user#logout"
+  get "new_post"=>"user#new_post"
  
 
   get '/'=>"home#top"
