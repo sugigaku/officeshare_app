c639c4427022d2c529ecf999d94a5bfaa4d1e3de
TaigaMikami <mikamimikami60@gmail.com>
2020-01-14 23:37:52 +0900

    トップページを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
-  get 'user/belongs'=>"user#belongs"
-  get "user/new_student"=>"user#new_student"
- post "user/create_student"=>"user#create_student"
- get "user/new_company"=>"user#new_company"
- post "user/create_company"=>"user#create_company"
-  get "user/:id"=>"user#mypage"
-  
-  
-  get "/login"=>"user#login_form"
-  post "/login"=>"user#login"
-  #postでログアウトできてないまま、getで暫定的に対応
-  post "/logout"=>"user#logout"
-  get "/logout"=>"user#logout"
-  get "new_post"=>"user#new_post"
- 
-
-  get '/'=>"home#top"
+  root '/'=>"home#top"
 
 
 
