79a76c9c0cd7225be8e50057887cc0adee2d875f
sugigaku <gakuya124ftb@gmail.com>
2020-04-20 13:46:38 +0900

    add acess permission for stagingapp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts << "minnanoheya.herokuapp.com"
+  config.hosts << "minnanoheya-stg2.herokuapp.com"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.hosts << "minnanoheya.herokuapp.com"
+  config.hosts << "minnanoheya-stg2.herokuapp.com"
 end
