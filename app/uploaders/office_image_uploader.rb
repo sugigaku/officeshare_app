e7e9c781b5faf0a7ec4bd9cecff382157cc7379c
sugigaku <gakuya124ftb@gmail.com>
2020-04-23 22:35:16 +0900

    change carrierwave.rb to fix error

--- app/uploaders/office_image_uploader.rb
+++ app/uploaders/office_image_uploader.rb
@@ -8,7 +8,7 @@ class OfficeImageUploader < CarrierWave::Uploader::Base
     storage :file
   else
     storage :fog
-  end  # storage :fog
+  end  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
