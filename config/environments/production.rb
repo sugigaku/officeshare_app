5c459420ed927424f6f147d8f450d6bf40beb516
sugigaku <gakuya124ftb@gmail.com>
2020-08-02 11:42:59 +0900

    change config.action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.hosts << "minnanoheya-stg2.herokuapp.com"
   config.hosts << "minnano-heya.herokuapp.com"
 
-  config.action_mailer.default_url_options = { :host => 'minnano-heya.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'minnanoheya.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
